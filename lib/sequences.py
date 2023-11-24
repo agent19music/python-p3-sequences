df5c10c1f2dffdf8a4f10559e75a26909208f7a8
agent19music <seanmotanya@gmail.com>
2023-11-24 19:00:56 +0300

    Done with assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list =[]
+    if length == 0:
+        return my_list
+    if length > 0 :
+        my_list.append(0)
+    if length > 1 :
+        my_list.append(1)
+    while len(my_list) < length:
+        my_list.append(my_list[-1] + my_list[-2])
+    return my_list
+
+pass
